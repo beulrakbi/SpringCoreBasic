704949ce2fe650fc45f5b869258e2b0e1c03ff6e
beulrakbi <iamjyh99@gmail.com>
2025-11-15 21:45:33 +0900

    스프링 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -7,24 +7,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-    //애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+// 애플리케이션의 설정 정보에 @Configuration 어노테이션 적어줌
+@Configuration
 public class AppConfig {
 
-    //생성자 주입
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
-    private static MemoryMemberRepository memberRepository() {
+    @Bean
+    public static MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-        //생성자 주입
+        @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
